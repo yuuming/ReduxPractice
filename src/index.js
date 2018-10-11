643cd49dc76aedb9264f10092fb2c19125847e94
Yuumi <yuuming.n@gmail.com>
2018-10-10 21:58:07 -0700

    Add youtube API KEY

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyAzA31IHCoOrvvzU8Yi5sFlnxiezSQvR98';
+const App = () => {
+    return <div>Hi!</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
