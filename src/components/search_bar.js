80048821eb5c18e505ce389349d982dc30cdfa1c
Yuumi <yuuming.n@gmail.com>
2018-10-11 19:50:17 -0700

    Create VideoList and VideoListItem class

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class SearchBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { term: ''};
-    }
-    render() {
-        return <input onChange={this.onInputChange} />;
-    }
+  constructor(props) {
+    super(props);
+    this.state = { term: '' };
+  }
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+      </div>
+    );
+  }
 
-    onInputChange(event) {
-      console.log(event.target.value);
-    }
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
 }
 
 export default SearchBar;
