a33d74862dc51997d66be9982261fdaaefb4be96
Yuumi <yuuming.n@gmail.com>
2018-10-11 17:48:06 -0700

    Create SearchBar class

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />;
-};
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { term: ''};
+    }
+    render() {
+        return <input onChange={this.onInputChange} />;
+    }
+
+    onInputChange(event) {
+      console.log(event.target.value);
+    }
+}
 
 export default SearchBar;
