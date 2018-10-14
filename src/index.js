6d976abcd30e55407ffa78ad95ebc5d682cae6d1
Yuumi <yuuming.n@gmail.com>
2018-10-13 23:53:41 -0700

    Implement debounce function

--- src/index.js
+++ src/index.js
@@ -27,9 +27,11 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((tern) => { this.videoSearch(tern)}, 300)
+
     return (
       <div>
-        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+        <SearchBar onSearchTermChange={videoSearch} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
